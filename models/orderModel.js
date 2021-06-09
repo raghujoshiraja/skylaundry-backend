04d471ebd26e82fadcc4383a9643aa09bbfe1627
anujwlv <84705118+anujwlv@users.noreply.github.com>
2021-06-09 11:31:13 +0100

    Update orderModel.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,32 +1,36 @@
 const mongoose = require('mongoose')
-
+ 
 const orderSchema = new mongoose.Schema({
-  categoryId: {
-    
-  },
-  clientId: {
-    type: String,
-    required: true,
-  },
-  allowerId: {
-    type: String
-  },
-  status: {
-    type: Number,
-    enum: [0, 1, 2], // 0 -> Pending, 1 -> Approved, 2 -> Rejected
-    default: 0
-  },
-  userMessage: {
-    type: String,
-    trim: true,
-    default: ""
-  },
-  allowerMessage: {
-    type: String,
-    trim: true,
-  }
+ clientId: {
+ type: String,
+ required: true,
+ },
+ driverId: {
+ type: String,
+ },
+ status: {
+ type: Number,
+ enum: [...Array(7 + 1).keys()], // Generates array from 0 to 7
+ // Order Status Number Meaning (initial 0)
+ // 0 = Ordered, waiting for alloting driver
+ // 1 = Driver allotted, ready for pick up
+ // 2 = Picked Up, ready to wash
+ // 3 = Washing (Processing wash cycle)
+ // 4 = Washed, Ready to be deliverd
+ // 5 = Delivery Driver Allotted
+ // 6 = Delivered by delivery Driver, payment pending by default
+ // 7 = Payment pending
+ // 8 = Payment Done
+ default: 0
+ },
+ order: {
+ // Array containing all orders in format
+ // [{categoryId: 'kahjsdfj', quantity: 10, price: ''}]
+ type: Array,
+ required: true,
+ }
 }, {
-  timestamps: true
+ timestamps: true
 })
-
+ 
 module.exports = mongoose.model('Orders', orderSchema)
