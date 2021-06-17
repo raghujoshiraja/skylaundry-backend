46cd3969ced485fd0e21dc0fddd4214d2d2093f2
raghujoshiraja <84302009+raghujoshiraja@users.noreply.github.com>
2021-06-17 17:33:57 +0530

    Update orderModel.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -30,30 +30,30 @@ const orderSchema = new mongoose.Schema(
       type: Array,
       required: true,
     },
+    driverId: {
+      type: String,
+    },
+    status: {
+      type: Number,
+      enum: [...Array(7 + 1).keys()], // Generates array from 0 to 7
+      // Order Status Number Meaning (initial 0)
+      // 0 = Ordered, waiting for alloting driver
+      // 1 = Driver allotted, ready for pick up
+      // 2 = Reached washing facility, ready to wash
+      // 3 = Washing (Processing wash cycle)
+      // 4 = Washed, Ready to be deliverd
+      // 5 = Delivery Driver Allotted
+      // 6 = Delivered by delivery Driver, payment pending by default
+      // 7 = Payment pending
+      // 8 = Payment Done
+      default: 0
+    },
+    order: {
+      // Array containing all orders in format
+      // [{categoryId: 'kahjsdfj', quantity: 10, price: ''}]
+      type: Array,
+      required: true,}
   },
-  driverId: {
-    type: String,
-  },
-  status: {
-    type: Number,
-    enum: [...Array(7 + 1).keys()], // Generates array from 0 to 7
-    // Order Status Number Meaning (initial 0)
-    // 0 = Ordered, waiting for alloting driver
-    // 1 = Driver allotted, ready for pick up
-    // 2 = Reached washing facility, ready to wash
-    // 3 = Washing (Processing wash cycle)
-    // 4 = Washed, Ready to be deliverd
-    // 5 = Delivery Driver Allotted
-    // 6 = Delivered by delivery Driver, payment pending by default
-    // 7 = Payment pending
-    // 8 = Payment Done
-    default: 0
-  },
-  order: {
-    // Array containing all orders in format
-    // [{categoryId: 'kahjsdfj', quantity: 10, price: ''}]
-    type: Array,
-    required: true,
   {
     timestamps: true,
   }
